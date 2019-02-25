1d3fdda7fb45ccdb20698a896c9ad188eb315b91
Philip Alekseev <phil.alekseev@gmail.com>
2019-02-24 22:20:59 -0500

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,40 @@
+// This is where project configuration and installed plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+module.exports = {
+  siteName: "Yearn Canada",
+  siteUrl: `https://www.yearn.ca`,
+  titleTemplate: "%s - Photography and Not-So-Safe Space",
+  siteDescription: "A place for me to express my mind and creativity and hopefully help people in the process.",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "projects/**/*.md",
+        typeName: "ProjectPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "journal/**/*.md",
+        typeName: "JournalPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+};
---
+++ journal/macos-development-environment.md
@@ -0,0 +1,12 @@
+---
+date: 2019-01-03 23:00:00 +0000
+title: macOS development environment
+author: Anon I. Moose
+excerpt: Get up and running with a fresh macOS Mojave install
+
+---
+### Install Homebrew
+
+    $ brew brew brew
+
+Enjoy your new system! 🎉
